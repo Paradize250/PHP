1ddcd64323a922db095bb9c8d0ba54a17f8033fc
¨csi <paradisemalcolm@gmail.com>
2023-09-15 11:20:32 +0200

    write variables

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-
 echo "Hello world from a php container"
 //comments
 /*comments on 
 multiple lines*/
-
-
 //variables
-
-
-
-?>
+$var = 'a variable';
+$Var = 'another variable';
+echo $var;
+echo $Var;
 
 
 
