45d06a7760c0ca4ee61200785c1cfa556e141928
¨csi <paradisemalcolm@gmail.com>
2023-09-15 11:49:10 +0200

    add items to the php code

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
+<h1> Welcome to php</h1>
 <?php
-echo "Hello world from a php container"
-//comments
-/*comments on 
-multiple lines*/
-//variables
+// variables use the dollar sign to declare a variable and always end with;
+
 $var = 'a variable';
 $Var = 'another variable';
+
 echo $var;
+echo '<br>';
 echo $Var;
+echo '<br>';
+$nb = (int) '5.6';
+echo '<br>';
+
+//printf($nb);
+var_dump($nb);
+echo '<br>';
+echo '<h2> title 2 </h2>';
+$nb = (bool) 'true';
+
+$camelcase = 'aVariableInCamelCase';
+$snake_case = 'a_variable_in_snake_case';
+$pascalcase = 'AnUppercaseOnEveryWord';
+
+$fruit1 = 'pomme';
+$fruit2 = 'banane';
+
+echo "fruit1" . "" . "fruit2";
+echo "<p>fruit1" . "" . "fruit2</p>";
+echo "<p>fruit1" . "<br>" . "fruit2</p>";
+echo '<br>';
+
+$resultat = '';
+$resultat .= 'The chosen fruit is:';
+
+?>
+
+
+
+
+
 
 
 
